c9b68915ec37a98f751e035bf4717b8713e6c67f
“khxdeejakhalid”~ <c21395463@mytudublin.ie>
2025-02-02 18:28:14 +0000

    Include maneuvers in routifyConstantsService

--- client/src/services/routifyConstantsService.js
+++ client/src/services/routifyConstantsService.js
@@ -1,15 +1,58 @@
 export const routifyConstantsService = (function () {
-    const goshoReminders = [
-      "Select gear",
-      "Observe surroundings",
-      "Signal",
-      "Handbrake down",
-      "Observation (over the shoulder)",
-    ];
-    const msmReminders = ["Check Mirrors", "Signal", "Manoeuvre/move"];
-  
-    return {
-      goshoReminders,
-      msmReminders,
-    };
-  })();
+  const GOSHO_REMINDERS = [
+    "Select gear",
+    "Observe surroundings",
+    "Signal",
+    "Handbrake down",
+    "Observation (over the shoulder)",
+  ];
+
+  const MSM_REMINDERS = ["Check Mirrors", "Signal", "Manoeuvre/move"];
+
+  const MANEUVERS_MAP = {
+    HILLTOP: "Hill Start",
+    REVERSE_PARKING: "Reverse Around the Corner",
+    PARALLEL_PARKING: "Turnabout",
+  };
+
+  const MANEUVERS = {
+    [MANEUVERS_MAP.HILLTOP]: {
+      startPos: {
+        lat: 53.2992462,
+        lng: -6.3492096,
+      },
+      endPos: {
+        lat: 53.2992462,
+        lng: -6.3492096,
+      },
+    },
+    [MANEUVERS_MAP.REVERSE_PARKING]: {
+      startPos: {
+        lat: 53.295606,
+        lng: -6.343768,
+      },
+      // @TODO: Needs to be figured out
+      endPos: {
+        lat: 0,
+        lng: -6.0,
+      },
+    },
+    [MANEUVERS_MAP.PARALLEL_PARKING]: {
+      startPos: {
+        lat: 53.296234,
+        lng: -6.341734,
+      },
+      // @TODO: Needs to be figured out
+      endPos: {
+        lat: 53.0,
+        lng: -6.0,
+      },
+    },
+  };
+  return {
+    GOSHO_REMINDERS,
+    MSM_REMINDERS,
+    MANEUVERS_MAP,
+    MANEUVERS,
+  };
+})();
