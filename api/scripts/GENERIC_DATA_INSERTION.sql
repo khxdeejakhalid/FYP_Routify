dec9ea86779f9a554ec5e47390a6b39199050e61
“khxdeejakhalid”~ <c21395463@mytudublin.ie>
2025-03-23 20:59:01 +0000

    Updated scripts for DB

--- api/scripts/GENERIC_DATA_INSERTION.sql
+++ api/scripts/GENERIC_DATA_INSERTION.sql
@@ -1,8 +1,48 @@
 -- TEST CENTERS DATA INSERTION
-INSERT INTO TEST_CENTERS VALUES (1, 'Tallaght', 'Dublin', 'Ireland');
+INSERT INTO
+    TEST_CENTERS
+VALUES
+    (1, 'Tallaght', 'Dublin', 'Ireland');
 
 -- ROUTES DATA INSERTION
-INSERT INTO ROUTES VALUES (1, 1, 'Route 1');
-INSERT INTO ROUTES VALUES (2, 1, 'Route 2');
-INSERT INTO ROUTES VALUES (3, 1, 'Route 3');
-INSERT INTO ROUTES VALUES (4, 1, 'Route 4');
+INSERT INTO
+    ROUTES
+VALUES
+    (1, 1, 'Route 1');
+
+INSERT INTO
+    ROUTES
+VALUES
+    (2, 1, 'Route 2');
+
+INSERT INTO
+    ROUTES
+VALUES
+    (3, 1, 'Route 3');
+
+INSERT INTO
+    ROUTES
+VALUES
+    (4, 1, 'Route 4');
+
+-- FEEDBACK DATA INSERTION
+INSERT INTO
+    FEEDBACK
+VALUES
+(1, 1, 'HILL_TOP', 4, 'khadeeja@gmail.com');
+
+INSERT INTO
+    FEEDBACK
+VALUES
+(
+        3,
+        1,
+        'REVERSE_AROUND_CORNER',
+        4,
+        'khadeeja@gmail.com'
+    );
+
+INSERT INTO
+    FEEDBACK
+VALUES
+(2, 1, 'TURN_ABOUT', 4, 'khadeeja@gmail.com');
