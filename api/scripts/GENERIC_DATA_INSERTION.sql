c168ba7e6e4528310c1efcc2e505650157845314
“khxdeejakhalid”~ <c21395463@mytudublin.ie>
2025-04-22 13:15:08 +0100

    Update DB with instructor and lesson tables/values

--- api/scripts/GENERIC_DATA_INSERTION.sql
+++ api/scripts/GENERIC_DATA_INSERTION.sql
@@ -1,3 +1,31 @@
+-- SAMPLE USER
+INSERT INTO
+    USERS
+VALUES
+    (
+        'Khadeeja',
+        'password',
+        'Khadeeja Khalid',
+        'khadeeja@gmail.com',
+        '2025-04-15',
+        '2003-11-25',
+        'learner'
+    );
+
+-- INSTRUCTOR DATA INSERTION
+INSERT INTO
+    USERS
+VALUES
+    (
+        'Admin',
+        'Admin123',
+        'George the Instructor',
+        'admin@gmail.com',
+        NULL,
+        '2025-04-02',
+        'instructor'
+    );
+
 -- TEST CENTERS DATA INSERTION
 INSERT INTO
     TEST_CENTERS
@@ -29,12 +57,12 @@ VALUES
 INSERT INTO
     FEEDBACK
 VALUES
-(1, 1, 'HILL_TOP', 4, 'khadeeja@gmail.com');
+    (1, 1, 'HILL_TOP', 4, 'khadeeja@gmail.com');
 
 INSERT INTO
     FEEDBACK
 VALUES
-(
+    (
         3,
         1,
         'REVERSE_AROUND_CORNER',
@@ -46,3 +74,35 @@ INSERT INTO
     FEEDBACK
 VALUES
 (2, 1, 'TURN_ABOUT', 4, 'khadeeja@gmail.com');
+
+-- Lessons Values
+INSERT INTO LESSONS VALUES (1, 'Lesson 1');
+INSERT INTO LESSONS VALUES (2, 'Lesson 2');
+INSERT INTO LESSONS VALUES (3, 'Lesson 3');
+INSERT INTO LESSONS VALUES (4, 'Lesson 4');
+INSERT INTO LESSONS VALUES (5, 'Lesson 5');
+INSERT INTO LESSONS VALUES (6, 'Lesson 6');
+INSERT INTO LESSONS VALUES (7, 'Lesson 7');
+INSERT INTO LESSONS VALUES (8, 'Lesson 8');
+INSERT INTO LESSONS VALUES (9, 'Lesson 9');
+INSERT INTO LESSONS VALUES (10, 'Lesson 10');
+INSERT INTO LESSONS VALUES (11, 'Lesson 11');
+INSERT INTO LESSONS VALUES (12, 'Lesson 12');
+
+--Lesson Feedback Values
+INSERT INTO
+    LESSON_FEEDBACK
+VALUES
+    (1, 'learner@gmail.com', 'admin@gmail.com', 'Great understanding of primary and secondary controls. Keep practicing cockpit drill ', 'completed'),
+    (2, 'learner@gmail.com', 'admin@gmail.com', 'Fantatstic steering and moving off smootly. However, more attention needed when moving off and checking mirrors!, ', 'completed'),
+    (3, 'learner@gmail.com', 'admin@gmail.com', 'Great technique when turning left, keep practising your right turns with your sponsor to avoid swan neck! ', 'completed'),
+    (4, 'learner@gmail.com', 'admin@gmail.com', 'Good attempt at junctions today. Sometimes hestiant at bigger junctions with filter lights, make sure to practise this otherwise good turns!', 'completed'),
+    (5, 'learner@gmail.com', 'admin@gmail.com', 'Handled small roundabouts really well and confidentally. Practice exiting roundabouts in your own time before we approach bigger roundabouts. Make sure to correctly observe and stay in the right lane!', 'completed'),
+    (6, 'learner@gmail.com', 'admin@gmail.com', NULL, 'scheduled'),
+    (7, 'learner@gmail.com', 'admin@gmail.com', NULL, 'scheduled'),
+    (8, 'learner@gmail.com', 'admin@gmail.com', NULL, 'pending'),
+    (9, 'learner@gmail.com', 'admin@gmail.com', NULL, 'pending'),
+    (10, 'learner@gmail.com', 'admin@gmail.com', NULL, 'pending'),
+    (11, 'learner@gmail.com', 'admin@gmail.com', NULL, 'pending'),
+    (12, 'learner@gmail.com', 'admin@gmail.com', NULL, 'pending');
+
